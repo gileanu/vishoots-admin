9ff7aa2296cf40bc15409110371800b4318aa49d
gileanu <gileanu@gmail.com>
2024-04-01 21:05:35 +0300

    [fix] dark mode destructive now looks better

---
+++ app/api/[portfolioId]/form/[categoryId]/route.ts
@@ -0,0 +1,113 @@
+import prismadb from "@/lib/prismadb";
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+export async function GET(
+  req: Request,
+  { params }: { params: { categoryId: string } }
+) {
+  try {
+    if (!params.categoryId) {
+      return new NextResponse("Category Id is required", { status: 400 });
+    }
+
+    const category = await prismadb.category.findUnique({
+      where: {
+        id: params.categoryId,
+      },
+    });
+
+    return NextResponse.json(category);
+  } catch (error) {
+    console.log("CATEGORY_GET", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
+
+export async function PATCH(
+  req: Request,
+  {
+    params,
+  }: {
+    params: { portfolioId: string; categoryId: string; categorySlug: string };
+  }
+) {
+  try {
+    const { userId } = auth();
+    const body = await req.json();
+    const { name, billboardId, categorySlug, imageUrl, categoryDesc } = body;
+    if (!userId) {
+      return new NextResponse("Unauthenticated", { status: 401 });
+    }
+    if (!name) {
+      return new NextResponse("Name is required", { status: 400 });
+    }
+    if (!params.categoryId) {
+      return new NextResponse("Category id is required", { status: 400 });
+    }
+
+    const portfolioByUserId = await prismadb.portfolio.findFirst({
+      where: {
+        id: params.portfolioId,
+        userId,
+      },
+    });
+
+    if (!portfolioByUserId) {
+      return new NextResponse("Not allowed", { status: 403 });
+    }
+
+    const category = await prismadb.category.updateMany({
+      where: {
+        id: params.categoryId,
+      },
+      data: {
+        name,
+        categorySlug,
+        imageUrl,
+        categoryDesc,
+      },
+    });
+    return NextResponse.json(category);
+  } catch (error) {
+    console.log("CATEGORY_PATCH", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
+
+export async function DELETE(
+  req: Request,
+  { params }: { params: { portfolioId: string; categoryId: string } }
+) {
+  try {
+    const { userId } = auth();
+    if (!userId) {
+      return new NextResponse("Unauthenticated", { status: 401 });
+    }
+    if (!params.categoryId) {
+      return new NextResponse("Category id is required", { status: 400 });
+    }
+
+    const portfolioByUserId = await prismadb.portfolio.findFirst({
+      where: {
+        id: params.portfolioId,
+        userId,
+      },
+    });
+
+    if (!portfolioByUserId) {
+      return new NextResponse("Not allowed", { status: 403 });
+    }
+
+    const category = await prismadb.category.deleteMany({
+      where: {
+        id: params.categoryId,
+      },
+    });
+
+    return NextResponse.json(category);
+  } catch (error) {
+    console.log("CATEGORY_DELETE", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
