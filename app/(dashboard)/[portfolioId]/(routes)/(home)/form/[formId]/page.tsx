d6451a7d80dc0c5aa803b143553e67717caa5851
gileanu <gileanu@gmail.com>
2024-04-04 20:03:14 +0300

    [live] first deployment

--- app/(dashboard)/[portfolioId]/(routes)/(home)/form/[formId]/page.tsx
+++ app/(dashboard)/[portfolioId]/(routes)/(home)/form/[formId]/page.tsx
@@ -1,8 +1,18 @@
+import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { HeadingH1 } from "@/components/ui/headingh1";
+import { Separator } from "@/components/ui/separator";
 
 import prismadb from "@/lib/prismadb";
 import { format } from "date-fns";
-import { CalendarCheck, Text, UploadCloud, User } from "lucide-react";
+import {
+  ArrowLeft,
+  CalendarCheck,
+  Text,
+  UploadCloud,
+  User,
+} from "lucide-react";
+import Link from "next/link";
 
 const contactPage = async ({
   params,
@@ -39,70 +49,91 @@ const contactPage = async ({
   }
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 gap-4 w-full mb-10">
-      <div className="space-y-4">
-        <Card className="shadow-lg">
-          <CardHeader className="flex flex-row items-center justify-between space-y-0">
-            <CardTitle>Submitted on:</CardTitle>
-            <UploadCloud className="h-6 w-6 text-sky-500" />
-          </CardHeader>
-          <CardContent>
-            <p className="text-md p-3 border rounded-md">
-              <span>{format(contact?.createdAt.toString()!, "PPPPpp")}</span>
-            </p>
-          </CardContent>
-        </Card>
-        <Card className="shadow-lg">
-          <CardHeader className="flex flex-row items-center justify-between space-y-0">
-            <CardTitle>Client info:</CardTitle>
-            <User className="h-6 w-6 text-green-500" />
-          </CardHeader>
-          <CardContent className="space-y-2">
-            <p className="text-sm text-muted-foreground">Name:</p>
-            <div className="md:text-md p-3 border rounded-md">
-              {contact?.name}
-            </div>
-            <p className="text-sm text-muted-foreground">Email:</p>
-            <div className="md:text-md p-3 border rounded-md underline">
-              <a href={`mailto:${contact?.email}`}>{contact?.email}</a>
-            </div>
-            <p className="text-sm text-muted-foreground">Phone number:</p>
-            <div className="md:text-md p-3 border rounded-md underline">
-              <a href={`tel:${contact?.phone}`}>{contact?.phone}</a>
-            </div>
-          </CardContent>
-        </Card>
-        <Card className="shadow-lg">
-          <CardHeader className="flex flex-row items-center justify-between space-y-0">
-            <CardTitle>Aditional info:</CardTitle>
-            <CalendarCheck className="h-6 w-6 text-rose-500" />
-          </CardHeader>
-          <CardContent className="space-y-2">
-            <p className="text-sm text-muted-foreground">Date of the event:</p>
-            <div className="md:text-md p-3 border rounded-md">
-              {formatDate(contact?.date)}
-            </div>
-            <p className="text-sm text-muted-foreground">Time of the event:</p>
-            <div className="md:text-md p-3 border rounded-md">
-              {formatTime(contact?.time)}
-            </div>
-          </CardContent>
-        </Card>
+    <>
+      <div className="flex items-center justify-between">
+        <HeadingH1
+          title="Form details"
+          desc="Make sure to respond via email or phone"
+        />
+        <Button asChild>
+          <Link href={`/`}>
+            <ArrowLeft className="mr-4 h-4 w-4" />
+            Go back
+          </Link>
+        </Button>
       </div>
-      <div className="space-y-4">
-        <Card className="shadow-lg">
-          <CardHeader className="flex flex-row items-center justify-between space-y-0">
-            <CardTitle>Message</CardTitle>
-            <Text className="h-6 w-6 text-orange-500" />
-          </CardHeader>
-          <CardContent>
-            <div className="md:text-md p-3 border rounded-md">
-              {contact?.message}
-            </div>
-          </CardContent>
-        </Card>
+      <Separator className="my-5" />
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 w-full mb-10">
+        <div className="space-y-4">
+          <Card className="shadow-lg">
+            <CardHeader className="flex flex-row items-center justify-between space-y-0">
+              <CardTitle>Submitted on:</CardTitle>
+              <UploadCloud className="h-6 w-6 text-sky-500" />
+            </CardHeader>
+            <CardContent>
+              <p className="text-md p-3 border rounded-md">
+                <span>
+                  {format(contact?.createdAt.toString()!, "PPPPpppp")}
+                </span>
+              </p>
+            </CardContent>
+          </Card>
+          <Card className="shadow-lg">
+            <CardHeader className="flex flex-row items-center justify-between space-y-0">
+              <CardTitle>Client info:</CardTitle>
+              <User className="h-6 w-6 text-green-500" />
+            </CardHeader>
+            <CardContent className="space-y-2">
+              <p className="text-sm text-muted-foreground">Name:</p>
+              <div className="md:text-md p-3 border rounded-md">
+                {contact?.name}
+              </div>
+              <p className="text-sm text-muted-foreground">Email:</p>
+              <div className="md:text-md p-3 border rounded-md underline">
+                <a href={`mailto:${contact?.email}`}>{contact?.email}</a>
+              </div>
+              <p className="text-sm text-muted-foreground">Phone number:</p>
+              <div className="md:text-md p-3 border rounded-md underline">
+                <a href={`tel:${contact?.phone}`}>{contact?.phone}</a>
+              </div>
+            </CardContent>
+          </Card>
+          <Card className="shadow-lg">
+            <CardHeader className="flex flex-row items-center justify-between space-y-0">
+              <CardTitle>Aditional info:</CardTitle>
+              <CalendarCheck className="h-6 w-6 text-rose-500" />
+            </CardHeader>
+            <CardContent className="space-y-2">
+              <p className="text-sm text-muted-foreground">
+                Date of the event:
+              </p>
+              <div className="md:text-md p-3 border rounded-md">
+                {formatDate(contact?.date)}
+              </div>
+              <p className="text-sm text-muted-foreground">
+                Time of the event:
+              </p>
+              <div className="md:text-md p-3 border rounded-md">
+                {formatTime(contact?.time)}
+              </div>
+            </CardContent>
+          </Card>
+        </div>
+        <div className="space-y-4">
+          <Card className="shadow-lg">
+            <CardHeader className="flex flex-row items-center justify-between space-y-0">
+              <CardTitle>Message</CardTitle>
+              <Text className="h-6 w-6 text-orange-500" />
+            </CardHeader>
+            <CardContent>
+              <div className="md:text-md p-3 border rounded-md">
+                {contact?.message}
+              </div>
+            </CardContent>
+          </Card>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
